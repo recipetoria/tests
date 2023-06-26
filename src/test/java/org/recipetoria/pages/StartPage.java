5f74c044f65fbd282a67e75859e5963b60153689
BayAzat <azatbay.job@gmail.com>
2023-06-26 10:15:18 +0500

    update StartPage.java

--- src/test/java/org/recipetoria/pages/StartPage.java
+++ src/test/java/org/recipetoria/pages/StartPage.java
@@ -2,6 +2,7 @@ package org.recipetoria.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class StartPage extends BasePage{
 
@@ -15,14 +16,19 @@ public class StartPage extends BasePage{
     }
 
     public StartPage avatarBtn() {
-        getDriver().findElement(By.cssSelector(".default-avatar")).click();
+        getDriver().findElement(By.cssSelector(".avatar__image_default")).click();
         return this;
     }
 
     public StartPage logoutBtn() {
-        getDriver().findElement(By.cssSelector(".profile-menu__btns > button:nth-of-type(3)")).click();
+        getWait2().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='log-out__btn']")));
+        getDriver().findElement(By.xpath("//button[@class='log-out__btn']")).click();
         return this;
     }
 
+    public StartPage okBtnModalWindow() {
+        getDriver().findElement(By.xpath("//button[@class='delete-account__ok']")).click();
+        return this;
+    }
 }
 
